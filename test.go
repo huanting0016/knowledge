da8fbb884942c735ddc97c2f86a7f82cfbb9728d
huanting0016 <1971265974@qq.com>
2021-09-09 21:37:13 +0800

    算法

--- test.go
+++ test.go
@@ -1,58 +1,32 @@
 package main
 
 /*
-输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，例如，如果输入如下4 X 4矩阵
+在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
 
-[[1,2,3,4],
-[5,6,7,8],
-[9,10,11,12],
-[13,14,15,16]]
-
-则依次打印出数字
-[1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
+输入：
+{1,2,3,3,4,4,5}
+返回值：
+{1,2,5}
 */
 
-func spiralOrder(matrix [][]int) []int {
-	if len(matrix) == 0 {
-		return nil
-	}
+func deleteDuplication(pHead *ListNode) *ListNode {
+	// write code here
+	res := &ListNode{}
+	res.Next = pHead
+	pre := res
+	for pHead != nil && pHead.Next != nil {
+		if pHead.Val == pHead.Next.Val {
+			for pHead.Next != nil && pHead.Val == pHead.Next.Val {
+				pHead.Next = pHead.Next.Next
+			}
+			pre.Next = pHead.Next
+		} else {
+			pre = pre.Next
+		}
 
-	var res []int
-	l, h, r, t := 0, 0, len(matrix[0])-1, len(matrix)-1
+		pHead = pHead.Next
 
-	for {
-		// 左-->右打印，打印完t++
-		for i := l; i <= r; i++ {
-			res = append(res, matrix[h][i])
-		}
-		h++
-		if h > t {
-			break
-		}
-		// 上-->下打印，打印完r--
-		for i := h; i <= t; i++ {
-			res = append(res, matrix[i][r])
-		}
-		r--
-		if l > r {
-			break
-		}
-		// 右-->左打印，打印完b--
-		for i := r; i >= l; i-- {
-			res = append(res, matrix[t][i])
-		}
-		t--
-		if h > t {
-			break
-		}
-		// 下-->上打印
-		for i := t; i >= h; i-- {
-			res = append(res, matrix[i][l])
-		}
-		l++
-		if l > r {
-			break
-		}
 	}
-	return res
+	return res.Next
 }
